66bdc8ec7a71358a29ac1d0d234c49d3b48328c2
Madhusudhan <madhusudhan.chandavare-gowda.887@my.csun.edu>
2025-02-21 11:23:54 -0800

    Done

---
+++ prj/Vivado/hw/srcs/top/hdl/top_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
+//Date        : Wed Feb  5 09:02:54 2025
+//Host        : madhusudhan-OMEN-by-HP-Gaming-Laptop-16-k0xxx running 64-bit Ubuntu 24.04.1 LTS
+//Command     : generate_target top_wrapper.bd
+//Design      : top_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module top_wrapper
+   ();
+
+
+  top top_i
+       ();
+endmodule
